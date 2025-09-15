349256ef91e024552b5e20c2fa76abf95453180c
FabienAstre <astrefabien@gmail.com>
2025-09-15 11:31:07 -0700

    get_stock_data.py

--- get_stock_data.py
+++ get_stock_data.py
@@ -1,50 +1,52 @@
 import os
 import datetime
-import urllib2
+import urllib.request  # <- Python 3 version of urllib2
 from dateutil.parser import parse
 import threading
 
+# Ensure the Quandl API key is set in your environment
 assert 'QUANDL_KEY' in os.environ
 quandl_api_key = os.environ['QUANDL_KEY']
 
-class nasdaq():
-	def __init__(self):
-		self.output = './stock_data'
-		self.company_list = './companylist.csv'
+class Nasdaq:
+    def __init__(self):
+        self.output = './stock_data'
+        self.company_list = './companylist.csv'
 
-	def build_url(self, symbol):
-		url = 'https://www.quandl.com/api/v3/datasets/WIKI/{}.csv?api_key={}'.format(symbol, quandl_api_key)
-		return url
+    def build_url(self, symbol):
+        url = f'https://www.quandl.com/api/v3/datasets/WIKI/{symbol}.csv?api_key={quandl_api_key}'
+        return url
 
-	def symbols(self):
-		symbols = []
-		with open(self.company_list, 'r') as f:
-			next(f)
-			for line in f:
-				symbols.append(line.split(',')[0].strip())
-		return symbols
+    def symbols(self):
+        symbols = []
+        with open(self.company_list, 'r') as f:
+            next(f)  # skip header
+            for line in f:
+                symbols.append(line.split(',')[0].strip())
+        return symbols
 
 def download(i, symbol, url, output):
-	print('Downloading {} {}'.format(symbol, i))
-	try:
-		response = urllib2.urlopen(url)
-		quotes = response.read()
-		lines = quotes.strip().split('\n')
-		with open(os.path.join(output, symbol), 'w') as f:
-			for i, line in enumerate(lines):
-				f.write(line + '\n')
-	except Exception as e:
-		print('Failed to download {}'.format(symbol))
-		print(e)
+    print(f'Downloading {symbol} {i}')
+    try:
+        with urllib.request.urlopen(url) as response:
+            quotes = response.read().decode('utf-8')  # decode bytes to string
+            lines = quotes.strip().split('\n')
+        # Ensure output directory exists
+        if not os.path.exists(output):
+            os.makedirs(output)
+        # Save CSV
+        with open(os.path.join(output, f"{symbol}.csv"), 'w') as f:
+            for line in lines:
+                f.write(line + '\n')
+    except Exception as e:
+        print(f'Failed to download {symbol}')
+        print(e)
 
 def download_all():
-	if not os.path.exists('./stock_data'):
-	    os.makedirs('./stock_data')
-
-	nas = nasdaq()
-	for i, symbol in enumerate(nas.symbols()):
-		url = nas.build_url(symbol)
-		download(i, symbol, url, nas.output)
+    nas = Nasdaq()
+    for i, symbol in enumerate(nas.symbols()):
+        url = nas.build_url(symbol)
+        download(i, symbol, url, nas.output)
 
 if __name__ == '__main__':
-	download_all()
+    download_all()
